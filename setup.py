6dda2fe73ee85a425b49e1f5656d40fae0480f03
mottodora <motoki@dna.bio.keio.ac.jp>
2016-06-09 19:15:54 +0900

    version 0.1.1に

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="indexGen",
-      version="0.1.0",
+      version="0.1.1",
       description="journal club index generator",
       author="motoki",
       author_email="motoki@dna.bio.keio.ac.jp",
